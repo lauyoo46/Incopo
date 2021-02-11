5cfdd553e44e7e0b1c2379fdce7615f46faee420
Laurentiu Ile <ilelaurentiu@yahoo.com>
2021-02-11 11:59:37 +0200

    Creates Login & Registration screens

---
+++ Incopo/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Incopo
+//
+//  Created by Laurentiu Ile on 10.02.2021.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let userCollection = "users".localized
+    static let email = "email".localized
+    static let firstName = "firstName".localized
+    static let lastName = "lastName".localized
+    static let recents = "recents".localized
+}
